b31f830ec8b359968e63d397ab3fb069d0739a08
Edward3103 <86288625+Edward3103@users.noreply.github.com>
2023-04-29 21:49:59 +0700

    categoryNav

--- client/src/components/CategoryNav.js
+++ client/src/components/CategoryNav.js
@@ -7,12 +7,19 @@ const CategoryNav = () => {
   return (
     <aside className="hidden xl:flex">
       <div className="bg-primary flex flex-col w-[286px] h-[500px] rounded-[8px] overflow-hidden">
-        <div className="bg-accent py-4 text-primary upercase">
+        <div className="bg-accent py-4 text-primary upercase font-semibold flex items-center justify-center">
           Browse Categories
         </div>
         <div className="flex flex-col gap-y-6 p-6">
           {data?.map((category) => {
-            return <Link>categories</Link>;
+            return (
+              <Link
+                to={`/products/${category.id}`}
+                className="cursor-pointer uppercase"
+                key={category.id}>
+                {category.attributes.title}
+              </Link>
+            );
           })}
         </div>
       </div>
