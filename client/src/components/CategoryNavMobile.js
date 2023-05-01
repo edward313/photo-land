8b57065f08c719cf2c1b5a56023d36731a9b9045
Edward3103 <86288625+Edward3103@users.noreply.github.com>
2023-05-01 18:04:28 +0700

    header-flex

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -1,7 +1,15 @@
-import React from 'react';
-
-const CategoryNavMobile = () => {
-  return <div>CategoryNavMobile</div>;
+import React from "react";
+import { FiX } from "react-icons/fi";
+const CategoryNavMobile = ({ setCatNavMobile }) => {
+  return (
+    <div className="w-full h-full bg-primary p-8 ">
+      <div
+        onClick={() => setCatNavMobile(false)}
+        className="flex justify-end mb-8 cursor-pointer">
+        <FiX className="text-3xl" />
+      </div>
+    </div>
+  );
 };
 
 export default CategoryNavMobile;
--- client/src/components/temp.js
+++ client/src/components/temp.js
@@ -14,47 +14,41 @@ import Cart from "../components/Cart.js";
 import { CartContext } from "../context/CartContext";
 const Header = () => {
   const { isOpen, setIsOpen } = useContext(CartContext);
-  const [catNavMobile, setCatNavMobile] = useState(false);
+  const [catNavMobile, setcatNavMobile] = useState(false);
   return (
-    <header className="bg-primary py-6 fixed w-full top-4 z-40 lg:relative xl:mb-[30px]">
+    <header className="bg-primary  py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px] ">
       <div className="container mx-auto">
-        <div className="flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0 ">
+        <div className="flex flex-row gap-4 lg:items-center justify-center mb-4 lg:mb-0">
           {/* menu */}
           <div
-            onClick={() => setCatNavMobile(true)}
+            onClick={() => setcatNavMobile(true)}
             className="text-3xl xl:hidden cursor-pointer">
             <FiMenu />
           </div>
+          {/* category nav mobile  */}
           <div
             className={`${
-              catNavMobile ? "left-0" : "-left-full"
+              catNavMobile ? "left-0" : "-left-full "
             } fixed top-0 bottom-0 z-30 w-full h-screen transition-all duration-200`}>
-            <CategoryNavMobile setCatNavMobile={setCatNavMobile} />
+            <CategoryNavMobile setcatNavMobile={setcatNavMobile} />
           </div>
           {/* logo */}
           <Link to={"/"}>
             <img src={Logo} />
           </Link>
-          <div className="hidden w-full xl:flex xl:max-w-[734px]">
+          {/* search form show only destop*/}
+          <div className="hidden w-full xl:flex xl:max-w-[734px] ">
             <SearchForm />
           </div>
-
-          {/* phone and cart */}
-          <div className="flex items-center gap-x-[10px]">
-            {/* phone */}
-            <div className="hidden xl:flex uppercase">
-              Need help ? 123 456 789{" "}
-            </div>
-            <div className="relative cursor-pointer">
-              <SlBag className="text-2xl" />
-              {/* amount */}
-              <div
-                className="bg-accent text-primary absolute w-[18px] h-[18px] rounded-full top-3 -right-1 text-[13px]
-               flex justify-center items-center font-bold tracking-[-0.1rem]">
-                2
-              </div>
-            </div>
+          <div className="flex items-center xl:flex xl:max-w-[734px]">
+            <div className="hidden xl:flex uppercase">Need help? 123456789</div>
             {/* cart */}
+            <div
+              onClick={() => setIsOpen(!isOpen)}
+              className="relative cursor-pointer">
+              <SlBag className="text-2xl" />
+              <div>2</div>
+            </div>
             <div
               className={`
             ${isOpen ? "right-0" : "-right-full"}
@@ -64,10 +58,10 @@ const Header = () => {
             </div>
           </div>
         </div>
-        {/* search form -show on mobile only */}
-        <div className="lg:hidden">
-          <SearchForm />
-        </div>
+      </div>
+      {/* searchform show only mobile */}
+      <div className="lg:hidden">
+        <SearchForm />
       </div>
     </header>
   );
